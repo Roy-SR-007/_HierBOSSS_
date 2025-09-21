5c32010874a60957b81f24412ad7ae1fa9dffa57
Somjit Roy <sroy_123@tamu.edu>
2025-09-20 20:29:54 -0500

    Update real_data_study.R

--- single_atom_data_analysis/real_data_study.R
+++ single_atom_data_analysis/real_data_study.R
@@ -1,13 +1,3 @@
-# =============================================================================
-# Copyright 2025. Somjit Roy and Pritam Dey.
-# This program implements HierBOSSS' application to single-atom catalysis dataset 
-# as developed in:
-#   Roy, S., Dey, P., Pati, D., and Mallick, B.K.
-#   'Hierarchical Bayesian Operator-induced Symbolic Regression Trees for Structural Learning of Scientific Expressions'.
-# Authors:
-#   Somjit Roy (sroy_123@tamu.edu) and Pritam Dey (pritam.dey@tamu.edu)
-# =============================================================================
-
 ### Single-Atom catalysis data
 
 load("single_atom_data_analysis/catalysis.rda")
